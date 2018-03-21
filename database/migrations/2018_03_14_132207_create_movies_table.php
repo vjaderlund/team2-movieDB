c8eedfe6c539326a9d714521a726f23891b5088a
Unknown <viktor.jaderlund@medieinstitutet.se>
2018-03-21 11:19:05 +0100

    Updated the movie table file for database.

--- database/migrations/2018_03_14_132207_create_movies_table.php
+++ database/migrations/2018_03_14_132207_create_movies_table.php
@@ -15,13 +15,16 @@ class CreateMoviesTable extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('Title');
+            $table->text('Picture')->nullable();
             $table->text('Description')->nullable();
             $table->integer('Runtime')->unsigned()->nullable();
             $table->date('ReleaseDate')->nullable();
             $table->integer('director_id')->unsigned();
             $table->integer('actor_id')->unsigned();
             $table->integer('users_id')->unsigned();
+
             $table->timestamps();
         });
     }
