519048369fd359f80aa7cbba2c2c04341bf497c5
Unknown <sach.radhika@gmail.com>
2018-03-23 15:48:37 +0100

    Routes for the actors and directors MVC part. folder and file name routes/web.php

---
+++ database/migrations/2018_03_22_204028_create_movies_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('movies', function (Blueprint $table) {
+          $table->increments('id');
+
+          $table->string('Title');
+          $table->text('Picture')->nullable();
+          $table->text('Description')->nullable();
+          $table->integer('Runtime')->unsigned()->nullable();
+          $table->date('ReleaseDate')->nullable();
+          $table->integer('director_id')->unsigned();
+          $table->integer('actor_id')->unsigned();
+          $table->integer('users_id')->unsigned();
+
+          $table->timestamps();
+    });
+  }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,20 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
+//Routes for the actor MVC part
+//created by Radhika Sonawane
+Route::get('/actors', 'actorController@index')->name('actors.index');
+Route::get('/actors/create', 'actorController@create')->name('actors.create');
+Route::post('/actors', 'actorController@store')->name('actors.store');
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/actors/{actor}/edit', 'actorController@edit')->name('actors.edit');
+Route::get('/actors/{actor}', 'actorController@show')->name('actors.show');
+Route::post('/actors/{actor}', 'actorController@update')->name('actors.update');
+
+Route::get('/directors', 'directorController@index')->name('directors.index');
+Route::get('/directors/create', 'directorController@create')->name('directors.create');
+Route::post('/directors', 'directorController@store')->name('directors.store');
+
+Route::get('/directors/{director}/edit', 'directorController@edit')->name('directors.edit');
+Route::get('/directors/{director}', 'directorController@show')->name('directors.show');
+Route::post('/directors/{director}', 'directorController@update')->name('directors.update');
